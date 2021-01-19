3cec0127518be662caac5b33d975f27c98357520
hyoilll <50327128+hyoilll@users.noreply.github.com>
2021-01-19 21:55:57 +0900

    URL namespace

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+# 테이블
+class Question(models.Model):
+    # 속성
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    # 함수
+    # 질문 text 반환
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+# 테이블
+class Choice(models.Model):
+    # 속성
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choice_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
+
+    # 함수
+    # 선택 text 반환
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,50 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
+# request - 클라이언트로부터 요청을 받음
+# HttpResponse - 요청받은 것을 처리하여 클라이언트에게 반환
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # 1
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    
+    # 2
+    # latest_question_list = Question.objects.order_by('pub_date')[:5]
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    # return HttpResponse(output)
+
+    # 3
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {'latest_question_list' : latest_question_list,}
+    # return HttpResponse(template.render(context, request))
+
+    # 4
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list' : latest_question_list,}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    # 1
+    # return HttpResponse("You're looking at question %s." % question_id)
+
+    # 2
+    # try:
+    #     question = Question.objects.get(pk = question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+
+    # 3
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question' : question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
